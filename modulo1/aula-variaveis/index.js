0d3a213713dfc32ffe084617d354944624ec16fd
luisedufraga <luisedufraga@gmail.com>
2022-02-07 21:20:22 -0300

    aula-variaveis

---
+++ modulo1/aula-variaveis/index.js
@@ -0,0 +1,56 @@
+/* Meu primeiro site, primeiro de muitos */
+
+
+console.log(" Meu primeiro projeto  !!")
+
+// const nome = prompt ("O que você quer cuzão??")
+
+
+
+
+/*
+const primeiroNome = " luis "
+const sobrenome = " fraga "
+const idade = 23
+const estudante = true 
+
+
+
+console.log(primeiroNome, " sou estudante?")
+console.log( sobrenome )
+console.log( idade ) 
+console.log( estudante)
+*/
+
+/*
+let novaVariavel = true
+console.log( typeof novaVariavel)
+*/
+/*
+const nomeDeUsuario = prompt( "qual seu nome ")
+
+
+const idadeDeUsuario = Number(prompt)( "qual sua idade")  // converter de texto para numero )
+
+ 
+
+const varString = 24// converter de numero para string
+
+
+
+console.log(typeof nomeDeUsuario)
+
+console.log(typeof idadeDeUsuario)
+*/
+
+
+
+
+const idadeTexto = "23"
+const idadeNumero = Number(idadeTexto)
+
+console.log(idadeTexto)
+console.log(idadeNumero)
+
+
+ 
