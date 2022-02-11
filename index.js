f7750f57251a60c17b9468e0a49dac16bd5af2fe
luisedufraga <luisedufraga@gmail.com>
2022-02-10 21:46:21 -0300

    Exercicio operadores

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+
+const nome = "Luis"
+const anoNascimento = 1998
+const anoAtual = 2022
+const idade2 = anoAtual - anoNascimento
+
+console.log("Nome:", nome)
+console.log("idade", idade2)
+console.log(idade2 >= 18)
+console.log(2050 - anoNascimento)
+*/
+/*
+const nome2 = "Luis"
+const idade2 = 18
+const negativado = true
+
+if //se
+    else //se não
+    */
+   //CONDICIONAIS
+
+
+   
