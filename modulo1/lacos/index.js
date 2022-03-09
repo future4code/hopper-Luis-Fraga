995d74e346f72fde183e7412af4ff4f215f78c2e
luisedufraga <luisedufraga@gmail.com>
2022-03-08 22:06:19 -0300

    Exercicio segunda

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,118 @@
+//Exercício de interpretação de código 
+//1- Se o i for inferior a 5 ele vai ficar no loop até parar, resultado 10
+//2- A- 19, 21, 23, 25, 27, 30.
+//2- B- Sim. .entries
+//3- Vai contar os asteriscos até a contagem chegar ao numero de linhas que o usuario colocou.
+
+//Exercício de escrita de código
+
+//1- 
+/*
+const usuario = prompt("Quantos bichinhos de estimação você tem?")
+const nomePet = prompt("Digite o nome dos pet")
+
+function armazenaNomes([obj]){
+nomePet = obj
+    if ( usuario <= 0 ) {
+        console.log(" Que pena! Você não pode adotar um pet!")
+        } else {
+            (obj > 0 )
+            while (obj > 0) {
+                console.log(obj)
+                obj++
+            }
+        }
+}
+     
+ */
+
+//2-
+//A-
+
+/*
+let usuarioArray = [10,20]
+
+function novoArray(array){
+    const novaArray = []
+    for (let i = 0; i < array.length; i++) {
+        console.log(primeiraArray) 
+}
+ 
+      
+  }
+
+
+*/
+
+//B
+/*
+let originalArray = 20
+
+function novoArray(array){
+    const novaArray = originalArray
+    for (let i = 0; i < array.length; i++) {
+        console.log(originalArray % 10) 
+
+}
+
+*/
+
+
+//C
+
+/*
+let originalArray = 20
+
+function novoArray(array){
+    const novaArray = originalArray
+    for (let i = 0; i < array.length; i++) {
+        console.log(originalArray.slice([início],[fim]) 
+
+
+        */
+//D
+/*
+const array = [30, 20]
+
+for(let i = 0; i < array.length; i++){
+    console.log(array.indexOf(30))
+}
+*/
+
+
+//E-
+/*
+
+let maiorNumero = 0
+let menorNumero = 0
+
+function pegarMaiorEMenorValor(array){
+    for(let numero of array){
+        console.log(numero)
+    }
+}
+
+const array = [30,50,1]
+pegarMaiorEMenorValor(array)
+
+
+*/
+
+
+//Tive muitas dificuldades, mas o plantão ajudou muito, quero continuar codando e práticando.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
