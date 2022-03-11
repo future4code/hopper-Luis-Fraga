2c9720cec913617666847736542a56125492ae35
luisedufraga <luisedufraga@gmail.com>
2022-03-10 21:56:14 -0300

    Exercício 2 da semana

---
+++ modulo1/callback/index.js
@@ -0,0 +1,233 @@
+//Exercício de intepretação de código
+//1- A- Vai imprimir o nome e o apelido de todos
+//2- Apenas os nomes
+//3- Vai filtrar apenas o item selecionado
+
+//Exercício de escrita de código
+//1-
+//A-
+/*
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+ const novoArray = pets.map((item) => {
+     return item.nome
+ })
+
+ console.log(novoArray)
+*/
+
+//B-
+/*
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+const novoArray = pets.filter((item) => {
+    
+   if (item.raca === "Salsicha"){
+    return item.raca
+   }
+    
+})
+
+console.log(novoArray)
+
+*/
+
+//C-
+/*
+const pets = [
+  { nome: "Lupin", raca: "Salsicha" },
+  { nome: "Polly", raca: "Lhasa Apso" },
+  { nome: "Madame", raca: "Poodle" },
+  { nome: "Quentinho", raca: "Salsicha" },
+  { nome: "Fluffy", raca: "Poodle" },
+  { nome: "Caramelo", raca: "Vira-lata" },
+];
+
+const desconto = pets
+  .filter((cachorro) => {
+    return cachorro.raca === "Poodle";
+  })
+  .map((poodle) => {
+    return `Você ganhou um cupom de desconto de 10% para tosar o/a ${poodle.nome}!`;
+  })
+
+  console.log(desconto)
+
+*/
+//2-
+//A-
+/*
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+
+const novoArray = produtos.map((item) => {
+     return item.nome
+ })
+
+ console.log(novoArray)
+
+*/
+//B-
+/*
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+
+const promocao = produtos.map((produto) => {
+    const desconto = (produto.preco * 5) / 100;
+    const novoPreco = produto.preco - desconto;
+    const objeto = {
+        nome: produto.nome,
+        preco: produto.preco.toFixed(2),
+    };
+    return objeto;
+});
+
+console.log(promocao)
+
+*/
+//C-
+
+/*
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+
+
+
+ const novoArray = produtos.filter((item) => {
+    
+    if (item.categoria === "Bebidas"){
+     return item.categoria
+    }
+     
+ })
+
+console.log(novoArray)
+
+*/
+
+//D-
+/*
+const produtos = [
+  { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+  { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+  { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+  { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+  { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+  { nome: "Cândida", categoria: "Limpeza", preco: 3.3 },
+  { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+  { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+  { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+  { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.8 },
+];
+/*
+const novoArray = produtos.filter((ype) => {
+  return ype.nome.includes("Ypê");
+});
+
+console.log(novoArray)
+*/
+
+//E-
+/*
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.3 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.8 },
+  ];
+
+  const produtosYpe = ype.map((ype) => {
+return (`Compre ${ype.nome} por ${ype.preco}`);
+  })
+
+  
+console.log(produtosYpe)
+*/
+
+//DESAFIO
+//A
+/*
+const pokemons = [
+    { nome: "Bulbasaur", tipo: "grama" },
+    { nome: "Bellsprout", tipo: "grama" },
+    { nome: "Charmander", tipo: "fogo" },
+    { nome: "Vulpix", tipo: "fogo" },
+    { nome: "Squirtle", tipo: "água" },
+    { nome: "Psyduck", tipo: "água" },
+ ]
+
+ const nomePoke = pokemons.map((poke) => poke.nome).sort()
+     console.log(nomePoke);
+ */
+/*
+//B-
+
+
+const pokemons = [
+  { nome: "Bulbasaur", tipo: "grama" },
+  { nome: "Bellsprout", tipo: "grama" },
+  { nome: "Charmander", tipo: "fogo" },
+  { nome: "Vulpix", tipo: "fogo" },
+  { nome: "Squirtle", tipo: "água" },
+  { nome: "Psyduck", tipo: "água" },
+];
+
+const arrayTipos = new Set(); //Object
+pokemons.map((poke) => {
+  arrayTipos.add(poke.tipo);
+});
+console.log(arrayTipos);
+*/
