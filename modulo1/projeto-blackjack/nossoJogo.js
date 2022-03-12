896d07018e1837b3165084db9a457ae9222747fa
luisedufraga <luisedufraga@gmail.com>
2022-03-11 21:32:46 -0300

    Projeto 15 dias

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,39 +10,51 @@
  * 
  * 
  */
-const carta = comprarCarta();
 
-const menssagem = "Boas vindas ao jogo Blackjack!";
-console.log(menssagem);
+console.log("Boas vindas ao jogo Blackjack!");
 
-let novaRodada = confirm("Quer iniciar uma nova rodada?");
+if (confirm("Quer iniciar uma nova rodada?")) {
+  const cartaComputador1 = comprarCarta();
+  const cartaComputador2 = comprarCarta();
+  const cartaUsuario1 = comprarCarta();
+  const cartaUsuario2 = comprarCarta();
+  const resultadoUsuario = cartaUsuario1.valor + cartaUsuario2.valor;
+  const resultadoComputador = cartaComputador1.valor + cartaComputador2.valor;
+  console.log(`As suas cartas foram: ${cartaUsuario1.texto} e ${cartaUsuario2.texto} pontuação final: ${resultadoUsuario}`)
+  console.log(`As suas cartas foram: ${cartaComputador1.texto} e ${cartaComputador2.texto} pontuação final: ${resultadoComputador}`)
+  if(resultadoUsuario === resultadoComputador) {
+console.log("Empate!")
+
+  }else if (resultadoUsuario > resultadoComputador) {
+    console.log("O usuário ganhou!")
+  }else if (resultadoComputador > resultadoUsuario) {
+    console.log("O computador ganhou!")
+  }
+  //console.log(`${resultadoUsuario === resultadoComputador} Empate!`)
+  //console.log(`${resultadoUsuario > resultadoComputador} O usuário ganhou!`)
+  //console.log(`${resultadoComputador > resultadoUsuario} O computador ganhou`)
+
+} else {
+ alert(`O jogo acabou bêbê!!`)
+}
+
+
+
 /*
-for (const i of novaRodada) {
-   i = carta
-  if (novaRodada === true){
-     novaRodada = carta
-     carta = carta.txt
-     carta.txt = carta.valor
-     console.log(`i ${novaRodada} ${carta.txt} ${carta.valor}` )
-  } else 
-  console.log("O jogo acabou!")
-
+if (novaRodada === true) {
+  console.log(`As suas cartas foram: ${resultadoUsuario} pontuação final: `);
+} else {
+  alert("O jogo acabou!");
 }
 */
+/*
+let novaRodada = confirm("Quer iniciar uma nova rodada?");
+console.log(novaRodada);
+if (novaRodada === true) {
+  
+console.log(`${cartaUsuario1} - carta: ${carta.valor}`)
 
-function iniciandoJogo(array) {
-  if (novaRodada === true) {
-    console.log(`Nova Rodada`);
-
-  } else novaRodada !== true;
-  {
-    console.log("O jogo acabou");
-  }
-
-  for (const array of novaRodada) {
-    carta = array;
-    console.log(carta.txt);
-    console.log(carta.valor);
-  }
+} else {
+  alert("O jogo acabou!");
 }
-
+*/
