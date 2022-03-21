289a88672eaaa5510f786925dd8930a268c978e5
luisedufraga <luisedufraga@gmail.com>
2022-03-20 21:25:59 -0300

    Projeto Sexta

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,42 +5,45 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return retornaTamanhoArray.length
+   return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  
+  return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+  return array.sort()
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+  return array.every()
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+ return array.every() / 2 
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  return array.apply()
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+if (num1 === num2){
+   return num1 + num2
+}
 
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   return n.exibePar()
 }
 
 // EXERCÍCIO 09
@@ -50,17 +53,17 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+  return array.object.programmers.map()
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   return filme.filter()
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+   return pessoa.map()
 }
 
 // EXERCÍCIO 13A
